ab251bda7fc74e77a17f73b6fa5a5a97cc0eaa5c
Shivanshu garg <gargshivanshu000@gmail.com>
2025-01-25 11:54:51 +0530

    This is the First Commit

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./dist/index.html"],
+  theme: {
+    extend: {
+      container:{
+        padding:'3rem',
+        center:true
+      }
+    },
+  },
+  plugins: [],
+}
+
